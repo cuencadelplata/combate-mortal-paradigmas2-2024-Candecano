a2ed2cb2ac55c77cf7b032d551c0e585a3ee649c
candecano11 <candecano11@gmail.com>
2024-08-16 01:07:47 +0000

    Add: ultimos cambios juego de la vida

--- src/main/java/com/example/demo/JuegoDeLaVida2.java
+++ src/main/java/com/example/demo/JuegoDeLaVida2.java
@@ -1,20 +1,43 @@
 package com.example.demo;
-import java.util.*;
-public class JuegoDeLaVida2 {
-   private List <String> caso1;
-   public JuegoDeLaVida2(){
-      caso1 = new ArrayList<String>();
-   }
-   public void Nace_una_celula_con_tres_vecinos(){
-      if (caso1.size()==3) {
-         caso1.add("Nueva Celula");
-      }
-        
-      }
-      public List<String> getcaso1(){
-         return caso1;
-      }
 
+public class JuegoDeLaVida2 {
+   private String caso1;
+    private String caso2;
+    private String caso3;
+    private String caso4;
+    private String caso5;
+
+    public String getCaso1() {
+        return caso1;
+    }
+    public void setCaso1(String valor) {
+        caso1 = valor;
+    }
+    public String getCaso2() {
+        return caso2;
+    }
+    public void setCaso2(String valor) {
+        caso2 = valor;
+    }
+    public String getCaso3() {
+        return caso3;
+    }
+    public void setCaso3(String valor) {
+        caso3 = valor;
+    }
+    public String getCaso4() {
+        return caso4;
+    }
+    public void setCaso4(String valor) {
+        caso4 = valor;
+    }
+    public String getCaso5() {
+        return caso5;
+    }
+    public void setCaso5(String valor) {
+        caso5 = valor;
+    }
+   
 
 
 }
