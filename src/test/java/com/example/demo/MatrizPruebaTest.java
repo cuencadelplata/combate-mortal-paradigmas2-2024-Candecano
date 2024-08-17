c46a3561f7eedf9bd5d9dcdc95727380758b2066
candecano11 <candecano11@gmail.com>
2024-08-17 23:52:16 +0000

    Add: prueba de matrices terminada

--- src/test/java/com/example/demo/MatrizPruebaTest.java
+++ src/test/java/com/example/demo/MatrizPruebaTest.java
@@ -2,30 +2,30 @@ package com.example.demo;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 
 public class MatrizPruebaTest{
- //1 vivo, 0 muerto
+ 
     @Test
-    void crear_matriz(){
-        int matriz [][]= {
+    void primer_fila_muere_uno_y_nace_otro(){
+        int matriz1 [][]= {
          {1,0,0},
          {0,1,1},
          {1,1,0}
         };
 
-        if (matriz [1][1]==1 && matriz[1][2]==1 && matriz [0][0] == 1){
+        if (matriz1 [1][1]==1 && matriz1[1][2]==1 && matriz1 [0][0] == 1){
 
-            matriz [0][0] = 0;
-            matriz [0][1] = 1;
+            matriz1 [0][0] = 0;
+            matriz1 [0][1] = 1;
            
-          assertEquals(0, matriz[0][0]);
-          assertEquals(1, matriz [0][1]);
+          
           
          
         }	
      
-        assertTrue(matriz[1][1]==1 && matriz[1][2]==1 && matriz [0][0] == 0 && matriz [0][1] == 1);
+        assertTrue(matriz1 [0][0] == 0 && matriz1 [0][1] == 1);
 
 
 
@@ -33,11 +33,33 @@ public class MatrizPruebaTest{
             
         }
          
+     
+     @Test
+     void segunda_fila_muere_uno_el_resto_queda_igual_nace_uno_tercer_fila(){
+
+      int matriz2 [][]= {
+        {0,1,0},
+        {0,1,1},
+        {1,1,0}
+       };
+       if (matriz2 [0][0]==0 && matriz2[1][0]==0 && matriz2 [1][2] == 1 ){
+         matriz2 [1][1] = 0;
+         matriz2[2][2] = 1;
+       }
+
+       assertTrue(matriz2[1][1]==0 );
+       assertTrue(matriz2[2][2]==1);
+
+    }
 
 
-        
+
+    
+    
+
+
+
 
 
 
     }
-    
