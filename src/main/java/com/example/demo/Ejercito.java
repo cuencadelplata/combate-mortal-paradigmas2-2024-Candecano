4e74b8c5b5818b18361617e97b8c76c4cacc7bad
Candela Cano Gimenez <candecano11@gmail.com>
2024-08-21 13:19:25 -0300

    Se agrega escudo al soldado y se pregunta si esta vivo a cada miembro del ejercito despues de cada combate

---
+++ src/main/java/com/example/demo/Ejercito.java
@@ -0,0 +1,93 @@
+package com.example.demo;
+
+public class Ejercito {
+    private int soldado;
+    private int tanque;
+    private int buque;
+    private int vida;
+    private int disparo;
+    private int vivo;
+    private int escudo; 
+
+
+    public Ejercito(int soldado, int tanque, int buque, int escudo ) {
+        this.soldado = soldado;
+        this.tanque = tanque;
+        this.buque = buque;
+        this.escudo = escudo;
+    }
+
+
+    public  void recibe(int disparo) {
+
+        if (escudo > 0) {
+            escudo -= disparo; 
+            if (escudo < 0) { 
+                setVida(getVida()+ escudo /2); 
+                escudo = 0; // El escudo se agota
+            }
+        } else {
+            setVida(getVida() - disparo); 
+        }
+        if (getVida() <= 0) {
+            setMuerto();
+        } else { 
+            setVivo(); 
+        }
+    }
+
+    public int getSoldado() {
+        return soldado;
+    }
+    public int getTanque() {
+        return tanque;
+    }
+    public int getBuque() {
+        return buque;
+    }
+
+    public boolean vidainicial(int vida) {
+        setVida(getVida()+vida);
+        return false;
+       }
+       public void setVida(int vida) {
+           this.vida = vida;
+       }
+
+    public int setDisparo(int i) {
+        return disparo = i;
+    }
+    public int getDisparo() {
+        return disparo;
+    }
+
+
+    public int getVida() {
+        return vida;
+    }
+
+    public boolean getMuerto() {
+        return getVida() == 0; 
+    }
+      
+
+    public int setMuerto() {
+        return 0;
+    }
+
+    public boolean getVivo() {
+        return getVida() > 0; 
+    }
+
+    public int setVivo() {
+        return vivo;
+    }
+    
+    public int getEscudo() {
+        return escudo;
+    }
+    public void setEscudo(int escudo) {
+        this.escudo = escudo;
+   
+    }
+}
---
+++ src/test/java/com/example/demo/EjercitoTest.java
@@ -0,0 +1,135 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercitoTest {
+    @Test
+  void test1() {
+    Ejercito buque1 = new  Ejercito(0,0,0, 0);
+    Ejercito soldado1 = new  Ejercito(0, 0, 0, 0);
+    Ejercito tanque1 = new  Ejercito(0, 0, 0, 0);
+    Ejercito disparo = new  Ejercito(0, 0, 0, 0);
+
+    assert buque1.getVida() == 0;
+    assert soldado1.getVida() == 0;
+    assert tanque1.getVida() == 0;
+    assert disparo.getVida() == 0;
+    
+
+    
+    }
+
+
+  @Test
+  void test2_vidas_iniciales() {
+    Ejercito soldado1= new Ejercito(0, 0, 0, 0);
+    Ejercito buque1=  new Ejercito(0, 0, 0, 0);
+    Ejercito tanque1= new Ejercito(0, 0, 0, 0);
+ 
+    soldado1.vidainicial(1);
+    tanque1.vidainicial(2);
+    buque1.vidainicial(3);
+  
+    assert soldado1.getVida()==1;
+    assert tanque1.getVida()==2;
+    assert buque1.getVida()==3;
+
+  }
+  @Test
+  void soldado_dispara_a_tanque_y_lo_mata() {
+    Ejercito soldado1= new Ejercito(0, 0, 0, 0);
+    Ejercito tanque1= new Ejercito(0, 0, 0, 0);
+   
+    tanque1.setVida(2);
+    soldado1.setDisparo(1);
+    tanque1.recibe(soldado1.getDisparo());
+    assert tanque1.getVida()==1;
+    assertTrue(tanque1.getVivo());
+
+
+    tanque1.setVida(1);
+    soldado1.setDisparo(1);
+    tanque1.recibe(soldado1.getDisparo());
+    assert tanque1.getVida()==0;
+    assertTrue(tanque1.getMuerto());
+  }
+  @Test
+  void  tanque_dispara_a_soladado_y_lo_mata() {
+    Ejercito soldado1= new Ejercito(0, 0, 0, 0);
+    Ejercito tanque1= new Ejercito(0, 0, 0, 0);
+   
+    soldado1.setVida(1);
+    tanque1.setDisparo(1);
+    soldado1.recibe(tanque1.getDisparo());
+    assert soldado1.getVida()==0;
+    assertTrue(soldado1.getMuerto());
+    }
+
+    @Test
+    void tanque_dispara_a_buque_y_lo_mata() {
+      Ejercito buque1=  new Ejercito(0, 0, 0, 0);
+      Ejercito tanque1= new Ejercito(0, 0, 0, 0);
+
+      buque1.setVida(3);
+      tanque1.setDisparo(1);
+      buque1.recibe(tanque1.getDisparo());
+      assert buque1.getVida()==2;
+      assertTrue(buque1.getVivo());
+  
+      buque1.setVida(2);
+      tanque1.setDisparo(1);
+      buque1.recibe(tanque1.getDisparo());
+      assert buque1.getVida()==1;
+      assertTrue(buque1.getVivo());
+
+      buque1.setVida(1);
+      tanque1.setDisparo(1);
+      buque1.recibe(tanque1.getDisparo());
+      assert buque1.getVida()==0;
+      assertTrue(buque1.getMuerto());
+  
+      }
+
+      @Test
+      void tanque_dispara_soladado_con_escudo() {
+      Ejercito soldado1= new Ejercito(0, 0, 0, 3);
+      Ejercito tanque1= new Ejercito(0, 0, 0, 0);
+  
+      soldado1.setVida(3); 
+      tanque1.setDisparo(1);
+      soldado1.recibe(tanque1.getDisparo());
+      assertEquals(2, soldado1.getEscudo()); 
+      assertEquals(3, soldado1.getVida()); 
+  
+      tanque1.setDisparo(1);
+      soldado1.recibe(tanque1.getDisparo());
+      assertEquals(1, soldado1.getEscudo()); 
+      assertEquals(3, soldado1.getVida()); 
+  
+      tanque1.setDisparo(1); 
+      soldado1.recibe(tanque1.getDisparo());
+      assertEquals(0, soldado1.getEscudo()); 
+      assertEquals(3, soldado1.getVida()); 
+  
+  
+      tanque1.setDisparo(3); // Disparo que  daña la vida 
+      soldado1.recibe(tanque1.getDisparo());
+      assertEquals(0, soldado1.getEscudo()); 
+      assertEquals(0, soldado1.getVida()); 
+   
+  
+    }
+
+
+
+
+
+
+
+
+}
